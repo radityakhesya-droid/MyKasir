53d24b46cd4398a1e8ad4b5545a479e1d1ea5d35
dizyy <radityakhesya@gmail.com>
2025-12-29 22:10:48 +0700

    fix: perbaikan jalur CSS dan input data awal

--- api/manage_products.php
+++ api/manage_products.php
@@ -58,7 +58,7 @@ $produk_query = mysqli_query($conn, "SELECT * FROM produk ORDER BY id ASC");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyKasir - Kelola Produk</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 
